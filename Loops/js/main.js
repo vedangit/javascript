7e0c3ff6824ab23f4b7dc3b50de4fec95f81b2b3
Vedangi Thokal <117835144+vedangit@users.noreply.github.com>
2024-01-15 10:42:08 +0530

    While and For loops

--- Loops/js/main.js
+++ Loops/js/main.js
@@ -1,1 +1,15 @@
 console.log("Hello World!");
+
+//while loop
+let myNumber = 0;
+
+while(myNumber < 5){
+  console.log(myNumber);
+  myNumber++;
+}
+
+
+//for loop
+for(let i = 0; i<=10; i++){
+  console.log(i);
+}
