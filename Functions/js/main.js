05e3f67f120fbd1f8a48c383ae7b28fd41feae12
Vedangi Thokal <117835144+vedangit@users.noreply.github.com>
2024-01-15 11:07:57 +0530

    Functions

--- Functions/js/main.js
+++ Functions/js/main.js
@@ -1,1 +1,21 @@
 console.log("Hello World!");
+
+//method -> in built functions
+//functions -> defined by users
+
+function sum(num1, num2){
+  return num1 + num2;
+}
+
+//function to get username from email
+function getUsername(email){
+  return email.slice(0, email.indexOf("@"));
+}
+
+const getUsername = function(email){
+    return email.slice(0, email.indexOf("@"));
+}
+
+const getUsername = (email) => {
+    return email.slice(0, email.indexOf("@"));
+}; //arrow funciton
