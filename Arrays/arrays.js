621ad73577f47636d56ce8a69e3f822a025bb48d
Vedangi Thokal <117835144+vedangit@users.noreply.github.com>
2024-01-20 20:11:08 +0530

    Create arrays.js

---
+++ Arrays/arrays.js
@@ -0,0 +1,12 @@
+const myArray = [2, 3, 4, "veda", 9,8];
+
+const myArray[5] = false;
+
+myArray.push("school"); //adds to array in the end, returns new length of array
+myArray.pop();
+myArray,unshift(42); //adds to array at front, returns new length of array
+myArray,shift(42); //removes front front
+
+delete myArray[0]; //will leave empty (undefined datatype) at that index
+
+myArray.splice(1, 1)
